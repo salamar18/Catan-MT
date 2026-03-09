984baed5b4199c8d982db5ca9f82c91501a51c6c
salamar18 <salamar@seas.upenn.edu>
2026-03-08 20:45:49 -0400

    basic file structure and starter code to run playwright locally. Also updated readme with workflow (#2)
    Co-authored-by: Sean Alamar <salamar@umich.edu>

---
+++ bot/bot.js
@@ -0,0 +1,16 @@
+const { chromium } = require('playwright');
+const startGame = require('./actions/startGame');
+
+(async () => {
+  const browser = await chromium.connectOverCDP('http://127.0.0.1:9222');
+
+  const context = browser.contexts()[0];
+  const pages = context.pages();
+  const page = pages.length > 0 ? pages[0] : await context.newPage();
+
+  await page.goto('https://colonist.io');
+
+  await startGame(page);
+
+  await page.pause();
+})();
